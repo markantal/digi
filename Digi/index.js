41d8f34ea393b61bfe6ff0900dac83c65a58adef
markantal <35372529+markantal@users.noreply.github.com>
2018-05-15 15:14:35 +0200

    Add files via upload

--- Digi/index.js
+++ Digi/index.js
@@ -1,4 +1,3 @@
-
 let people = [
     'firstname',
     'phone',
@@ -9,13 +8,13 @@ let people = [
     'postcode',
 ];
 
-people.firstname = ['Christian','Rich','Scott','Danny','Taka','Tim','Patrick','Jacques'];
-people.phone = ['323-555-1234','323-555-1234','555-555-5555','323-555-1234','323-555-1234','323-555-1234','323-555-1234','323-555-1234',];
-people.email = ['christian@yahoo.com','rich@tripod.com','scott@mailnator.com','danny@hotmail.com','taka@myspace.com','tim@netscape.com','patrick@live.com','jacques@aol.com'];
-people.address = ['123 street','123 street','123 street','123 street','123 street','123 street','123 street','123 street'];
-people.city = ['big city','big city','big city','big city','big city','big city','big city','big city'];
-people.province = ['WC','WC','WC','WC','WC','WC','WC','WC'];
-people.postcode = ['8000','8000','8000','8000','8000','8000','8000','8000'];
+people.firstname = ['Christian', 'Rich', 'Scott', 'Danny', 'Taka', 'Tim', 'Patrick', 'Jacques'];
+people.phone = ['323-555-1234', '323-555-1234', '555-555-5555', '323-555-1234', '323-555-1234', '323-555-1234', '323-555-1234', '323-555-1234', ];
+people.email = ['christian@yahoo.com', 'rich@tripod.com', 'scott@mailnator.com', 'danny@hotmail.com', 'taka@myspace.com', 'tim@netscape.com', 'patrick@live.com', 'jacques@aol.com'];
+people.address = ['123 street', '123 street', '123 street', '123 street', '123 street', '123 street', '123 street', '123 street'];
+people.city = ['big city', 'big city', 'big city', 'big city', 'big city', 'big city', 'big city', 'big city'];
+people.province = ['WC', 'WC', 'WC', 'WC', 'WC', 'WC', 'WC', 'WC'];
+people.postcode = ['8000', '8000', '8000', '8000', '8000', '8000', '8000', '8000'];
 
 
 const addPeople = document.querySelector('#formUpdate');
@@ -31,10 +30,10 @@ const tdMaker = (text) => {
     const td = document.createElement('td');
     td.textContent = text;
     tr.appendChild(td);
-  }
+}
 
-function addContact(firstname,email,phone,address,city,province,postcode) {
-    
+function addContact(firstname, email, phone, address, city, province, postcode) {
+
     people.push({
         "firstname": firstname,
         "email": email,
@@ -46,27 +45,26 @@ function addContact(firstname,email,phone,address,city,province,postcode) {
 
     });
     document.getElementById('alert').innerHTML = "Successfully addes!";
-  }
+}
 
 
 function selection() {
-    var selected=document.getElementById("EmailOrPhone");
-     if(selected[selected.selectedIndex].value == "email") {
-        document.getElementById("tblEmail").style.display="table";
-        document.getElementById("tblPhone").style.display="none";
-      
-     } else
+    var selected = document.getElementById("EmailOrPhone");
+    if (selected[selected.selectedIndex].value == "email") {
+        document.getElementById("tblEmail").style.display = "table";
+        document.getElementById("tblPhone").style.display = "none";
 
-     if(selected[selected.selectedIndex].value == "phone"){
-        document.getElementById("tblEmail").style.display="none";
-        document.getElementById("tblPhone").style.display="table";
-     }
+    } else
 
+    if (selected[selected.selectedIndex].value == "phone") {
+        document.getElementById("tblEmail").style.display = "none";
+        document.getElementById("tblPhone").style.display = "table";
     }
-  
-//Delete function
-function delete_contact(firstname){
-    contacts.remove("firstname", name);
-    document.getElementById("status").innerHTML ="Successfully deleted.";
+
 }
 
+//Delete function
+function delete_contact(firstname) {
+    contacts.remove("firstname", name);
+    document.getElementById("status").innerHTML = "Successfully deleted.";
+}
